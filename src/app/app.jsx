e966803c6468e23adcada4260ef3d02277988f04
Orpheon <alex.liebendorf@gmail.com>
2019-03-09 23:04:20 +0100

    Fixed broken merge thing.

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -204,41 +204,4 @@ backendSocket.on('data', (msg) => {
 
 backendSocket.on('user id', (msg) => {
   console.log(`your user id: ${msg}`);
-==== BASE ====
-});
-
-
-const sensorSocket = new WebSocket('ws://130.82.239.210/ws');
-
-sensorSocket.onopen = function() {
-  console.log("sensor socket was opened");
-  let a = JSON.stringify({
-    signals: [
-      {
-        Name: "ESP_Laengsbeschl",
-      },
-      {
-        Name: "ESP_Querbeschleinigung",
-      },
-    ],
-    samplerate: 250,
-    withtimestamp: true
-  });
-  console.log(a);
-  sensorSocket.send(a);
-};
-
-sensorSocket.onmessage = (e) => { 
-  var received_msg = e.data;
-  console.log(`received message: ${received_msg}`);
-  backendSocket.emit('raw data', received_msg);
-};
-
-sensorSocket.onerror = (e) => {
-  console.log("Socket error: " + e.data);
-};
-
-sensorSocket.onclose = (e) => {
-  console.log("Socket close: ", e);
 }
-==== BASE ====
